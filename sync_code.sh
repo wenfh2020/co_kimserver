dfa577ec415260dd38075d57b801eceaa67cc99c
wenfh2020 <wenfh2020@126.com>
2021-01-21 10:18:55 +0800

    优化跨平台源码同步脚本。

--- sync_code.sh
+++ sync_code.sh
@@ -12,10 +12,12 @@ rsync -avz --exclude="*.o" \
     --exclude="*.log" \
     --exclude="config.json" \
     --exclude="co_kimserver" \
+    --exclude="test/test_hiredis/test_hiredis" \
     --exclude="test/test_log/test_log" \
+    --exclude="test/test_mysql/test_mysql" \
     --exclude="test/test_mysql_mgr/test_mysql_mgr" \
+    --exclude="test/test_redis_mgr/test_redis_mgr" \
     --exclude="test/test_tcp/test_tcp" \
     --exclude="test/test_tcp/test_tcp_pressure" \
-    --exclude="test/test_mysql/test_mysql" \
     --exclude="test/test_timer/test_timer" \
     ~/src/other/coroutine/co_kimserver/ root@wenfh2020_sgx.com:/home/other/coroutine/co_kimserver/
